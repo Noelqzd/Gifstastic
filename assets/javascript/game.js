13aadc028cfda5079c31424be86a20a9671cb2b6
Noel Quezada <noelquezada@gmail.com>
2019-08-04 18:57:14 -0700

    last commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,99 @@
-var myData = ["apollo intensa emozione", "jetstream sc250", "aspid ifr", "mclaren", "porshe 718 boxter", "audi r8", "ferrari f50 spider", "maserati alfieri", "tvr chimaera", "lamborghini huracan", "koenigsegg jesko", "gta spano", "buggati chiron", "ariel atom"];
+let myData = ["Jetstream SC250", "Aspid Ifr", "McLaren", "Porshe", "Audi R8", "Ferrari F50 Spider", "Maserati Alfieri", "TVR Chimaera", "LamborghinI", "Koenigsegg Jesko", "GTA Spano", "Buggati Chiron", "Ariel Atom"];
 
-$(document).ready(function () {
+$(document).ready(function() {
     renderButton();
     function renderButton() {
         $("#allbuttons").empty();
 
-        for (var index = 0; index < myData.length; index++) {
-            console.log(myData);
+        for (let i=0 ; i < myData.length; i++) {
 
             let newButton = $("<button>");
             newButton.addClass("itembutton");
-            newButton.addClass("btn btn-succes");
-            newButton.text(myData[index]);
-            newButton.attr("data-name", myData[index]);
+            newButton.addClass("btn btn-success");
+            newButton.text(myData[i]);
+            newButton.attr("data-name", myData[i]);
             $("#allbuttons").append(newButton);
-        
+
         }
+
     }
-});
+
+
+
+    $("#addbutton").on("click",  function(event) {
+        
+        event.preventDefault();
+        let addedData = $("#userinput").val().trim();
+        if (addedData != "") {
+            myData.push(addedData);
+            renderButton();
+            $("#userinput").val(" ");
+        }
+
+
+
+    });  
+
+    $(document).on("click", ".itembutton", displayInfo);
+
+
+    function displayInfo() {
+        let itemName = $(this).attr("data-name");
+        let queryURL = "https://api.giphy.com/v1/gifs/search?q=cars+" + itemName + "&rating=g&limit=15&api_key=1mqaRI16fveNQPuJoJIyqm6x304W02s3";
+        $("#mainimages").empty();
+
+        $.ajax ({
+            url: queryURL,
+            method: "GET"
+        }) .then(function(response) {
+            console.log(response);
+
+            let results = response.data;
+
+            for (let i=0; i<results.length; i++) {
+
+                let dataImage = $("<img>");
+                dataImage.attr("src", results[i].images.fixed_height_still.url);
+                dataImage.attr("data-still", results[i].images.fixed_height_still.url);
+                dataImage.attr("data-animate", results[i].images.fixed_height.url);
+                dataImage.addClass("gif");
+                dataImage.attr("data-state", "still");
+    
+    
+                let newItemdiv = $('<div class="newItem">');
+                let gifRating = results[i].rating;
+                let divRating = $("<p>").text("Rating: " + gifRating);
+                
+                newItemdiv.append(divRating);
+                newItemdiv.append(dataImage);
+    
+                $("#mainimages").prepend(newItemdiv);
+
+
+
+            }
+    
+ 
+        }); 
+
+
+    }
+
+
+    $("#mainimages").on("click", ".gif", function() {
+        let state = $(this).attr("data-state");
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        }
+
+
+        else if (state === "animate") {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+
+    });
+
+
+})
